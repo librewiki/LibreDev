3ef69a1986d1fa4ab9875495444a2e22ca2e84c0
senior9324 <senior9324@gmail.com>
2015-08-07 15:03:52 +0900

    VoteNY 확장기능 수정해서 추가

---
+++ extensions/VoteNY/VoteNY.alias.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Aliases for special pages
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = array();
+
+$specialPageAliases['en'] = array(
+	'TopRatings' => array( 'TopRatings' ),
+);
+
+$specialPageAliases['ko'] = array(
+	'TopRatings' => array( '평가높은문서' ),
+);
